02817fadaad839a7c9d5deb897eb47103272b864
roshanrajgd <90679688+roshanrajgd@users.noreply.github.com>
2021-11-01 00:21:49 -0700

    blanke code of pc engine

---
+++ pro-c20-ta-boilerplate-main/sketch.js
@@ -0,0 +1,24 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+
+var engine;
+var world;
+
+function setup(){
+  createCanvas(400,400);
+  engine = Engine.create();
+  world = engine.world
+}
+
+function draw() 
+{
+  background(51);
+  Engine.update(engine);
+ 
+}
+
+
+ 
